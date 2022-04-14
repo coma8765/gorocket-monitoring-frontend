a2066423a9a97028d5283a5c5e74942bdb09aabd
unknown <coma8765@T_OFFICE_4_3.COD40.com>
2022-04-14 14:57:23 +0300

    init commit

---
+++ shared/api.js
@@ -0,0 +1,14 @@
+export const getOldMeasures = async () => {
+    return await (await fetch(`http://${window.location.hostname}:8000/messages`, {
+        method: "GET",
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    })).json()
+}
