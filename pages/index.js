63a20f5773d9a5f4f4aeaf164e13a4f21fb32436
unknown <coma8765@T_OFFICE_4_3.COD40.com>
2022-04-14 14:57:11 +0300

    init commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,137 @@
-import Head from 'next/head'
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+import {getOldMeasures} from "../shared/api";
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export const options = {
+    responsive: true,
+    animation: {
+        duration: 2, // general animation time
+    },
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+    },
+    scales: {
+        x: {
+            grid: {
+               display: false
+           }
+        },
+        y: {
+            grid: {
+                display: false
+            }
+        },
+        xAxes: [{
+            type: 'linear',
+            position: 'bottom',
+            ticks: {
+                max: 12,
+                min: 1,
+                stepSize: 1,
+                callback: function(value, index, values) {
+                    return 1;
+                }
+            }
+        }]
+    },
+    annotation: {
+        annotations: [
+            {
+                drawTime: "afterDatasetsDraw",
+                type: "line",
+                mode: "vertical",
+                scaleID: "x-axis-0",
+                value: 3.8,
+                borderWidth: 5,
+                borderColor: "red",
+                label: {
+                    content: "TODAY",
+                    enabled: true,
+                    position: "top"
+                }
+            }
+        ]
+    },
+};
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+export default function App(callback, deps) {
+    const [rawData, setRawData] = useState([]);
+    const [WSState, setWSState] = useState(false);
+    const ws = useRef(null);
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    useEffect(async () => setRawData((
+        (await getOldMeasures())
+            .filter(v => v.IS_STARTED)
+            // .filter((_, i) => i % Math.sqrt(i) % 8 === 0)
+    )), []);
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    useEffect(() => {
+        ws.current = new WebSocket(`ws://${window.location.hostname}:8000/ws`); // создаем ws соединение
+        ws.current.onopen = () => setWSState("Соединение открыто");  // callback на ивент открытия соединения
+        ws.current.onclose = () => setWSState("Соединение закрыто"); // callback на ивент закрытия соединения
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        gettingData();
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        return () => ws.current.close(); // кода меняется isPaused - соединение закрывается
+    }, [ws]);
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+    const gettingData = useCallback(() => {
+        if (!ws.current) return;
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+        ws.current.onmessage = e => {                //подписка на получение данных по вебсокету
+            const messages = JSON.parse(e.data).data;
+                // .filter(v => v.IS_STARTED);
+            setRawData(s => [...s, ...messages]); //
+        };
+    }, []);
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+    const someData = rawData
+        .slice(-250, -1)
+        // .filter((_, i) => i % 4 === 0).slice(-50, -1)
+    const data = {
+        labels: someData.map(v => ""), //!v.IS_STARTED ? "PENDING" : "Started") ,
+        datasets: [
+            {
+                label: "Altitude",
+                data: someData.map(v => v.ALTITUDE),
+                borderColor: 'green',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            }, {
+                label: "Voltage",
+                data: someData.map(v => v.VOLTAGE),
+                borderColor: 'red',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            }, {
+                label: "Absolute vector",
+                data: someData.map(v => v.VECTOR_ABS),
+                borderColor: 'blue',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            }
+        ]
+    };
 
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+    return <Line options={options} data={data} />;
 }
